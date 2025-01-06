516a96a685c714356886896bac2db0245861909b
giorodriguss <irodrigues708@gmail.com>
2025-01-05 21:25:14 -0300

    Back: conexão com o banco de dados e rota de teste

---
+++ db.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+  console.log("Conectando ao Banco de Dados...");
+
+  mongoose.connect( process.env.MONGO_URI )
+  .then(() => console.log("MongoDB Atlas conectado!"))
+  .catch((error) => console.log(error));
+}
+
+module.exports = connectDatabase; 
--- index.js
+++ index.js
@@ -2,32 +2,33 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const connectDatabase = require('./db');
+const User = require('./models/User');
 
 const app = express();
+
+connectDatabase()
 
 app.use(morgan(':method :url - Status :status - Tempo: :response-time ms'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
-const data = [];
+app.post('/users', async (req, res) => {
+    try {
+        const { nome, idade, email, senha } = req.body;
+        if (!nome || !idade || !email || !senha) {
+            return res.status(400).json({ error: 'Todos os campos são obrigatórios.' });
+            }
+            
+        const newUser = new User({ nome, idade, email, senha });
+        await newUser.save();
 
-app.get('/', (req, res) => {
-    return res.json({ data });
-});
-
-app.post('/add', (req, res) => {
-    const result = req.body;
-
-    if (result) {
-        return res.status(400).json({ error: 'Dados não fornecidos' });
-        };
-      
-
-    data.push(result);
-    return res.status(200).json({ result });
-});
-
-app.listen(3000, () => 
-    console.log('Servidor rodando em http//localhost:3000')
-    );
+        res.status(201).json({ message: "Usuário criado com sucesso", user: newUser });
+         } catch(error) {
+            console.error("Erro ao criar usuário:", error);
+            res.status(500).json({error: "Erro ao criar usuário."});
+         }
+        });
+    
+app.listen(3000, () => console.log('Servidor rodando em http//localhost:3000'));
